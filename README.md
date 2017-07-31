3dffc5d491a88c9c320f3ddf55253aed815c6a97
Tanner Welsh <tannerwelsh@gmail.com>
2017-07-31 17:42:48 -0400

    Can generate + download reports

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 A Google Chrome extension that converts coding challenge gists with weighted requirements into a form that can be used to calculate the total score for a challenge.
 
+Once the points have been determined for each requirement, you can generate and download a JSON report.
+
 In short, it turns this:
 
 ![before](https://user-images.githubusercontent.com/709100/28796901-4d87fc3a-760d-11e7-841b-535841d42abf.png)
 
 into this:
 
-![after](https://user-images.githubusercontent.com/709100/28798464-8e778986-7612-11e7-9cd4-f36cceb0fb00.png)
-
+![after](https://user-images.githubusercontent.com/709100/28799671-8a014522-7617-11e7-8ec2-17644dc295ca.png)
 
 ## Installation
 
--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+// Pattern to match for requirement checkboxes
+const MATCH_REQ = /^\s*(\d+): (.+)$/
+
+// Element selectors
 const select = {
   taskListItems: () => document.querySelectorAll('li.task-list-item'),
   reqPoints: () => document.querySelectorAll('.req-points'),
@@ -6,6 +10,7 @@ const select = {
   denominator: () => document.querySelector('.points-denominator'),
   earned: () => document.querySelector('.points-earned'),
   percent: () => document.querySelector('.points-percent'),
+  learnerUsername: () => document.querySelector('input[name="learnerUsername"]'),
 }
 
 function insertFooter() {
@@ -21,23 +26,28 @@ function insertFooter() {
   footer.style['padding'] = '12px'
   footer.innerHTML = `
     <strong>Challenge Evaluation Calculator</strong> &nbsp; | &nbsp;
-    Points total:
+    Points:
     <span class="points-earned">0</span> / <span class="points-denominator">0</span>
-    (<span class="points-percent">0</span>%)
+    (<span class="points-percent">0</span>%) &nbsp; | &nbsp;
+    <input type="text" name="learnerUsername" placeholder="Learner username">
   `
+
+  const reportGeneratorButton = document.createElement('button')
+  reportGeneratorButton.innerText = 'Make Report'
+  reportGeneratorButton.addEventListener('click', generateReport)
+
+  footer.appendChild(reportGeneratorButton)
 
   document.body.appendChild(footer)
 }
 
 function replaceCheckboxesWithInputs() {
-  const matchReq = /^ (\d+): (.+)$/
-
   select.taskListItems().forEach((elem) => {
     const itemText = elem.innerText
 
-    if (matchReq.test(itemText)) {
+    if (MATCH_REQ.test(itemText)) {
       const checkbox = elem.querySelector('input')
-      const value = itemText.match(matchReq)[1]
+      const value = itemText.match(MATCH_REQ)[1]
       const input = makeInput(value)
 
       elem.prepend(input)
@@ -83,6 +93,44 @@ function calculatePointsEarned(event) {
 
 function toPercent(decimal) {
   return Math.round(decimal * 100 * 100) / 100
+}
+
+function generateReport() {
+  const report = {
+    username: select.learnerUsername().value,
+    pointsEarned: select.earned().innerText,
+    totalPoints: select.denominator().innerText,
+  }
+
+  const rawSections = []
+  let currentSection
+
+  document.querySelectorAll('h2, li.task-list-item').forEach((elem) => {
+    const text = elem.innerText
+
+    switch (elem.tagName) {
+      case 'H2':
+        currentSection = { section: text, reqs: [] }
+        rawSections.push(currentSection)
+        break;
+      case 'LI':
+        if (MATCH_REQ.test(text)) {
+          const [maxValue, reqText] = text.match(MATCH_REQ).slice(1)
+          const givenValue = elem.querySelector('input.req-points').value
+          currentSection.reqs.push(`[${givenValue} / ${maxValue}] : ${reqText}`)
+        }
+        break;
+    }
+  })
+
+  report.sections = rawSections.filter((section) => section.reqs.length > 0)
+
+  // Generate a file for the report and download it using a temporary, invisible link
+  const tempLink = document.createElement('a')
+  const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(report));
+  tempLink.href = dataStr
+  tempLink.download = report.username.length > 0 ? `${report.username}.json` : 'report.json'
+  tempLink.click()
 }
 
 // Load and execute
